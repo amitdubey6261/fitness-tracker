214b1c9dc7547a32d5c469520d51a4fcc67aeabe
amit dubey <amitdubey6261@gmail.com>
2023-05-16 00:40:28 +0530

    fix-cam

--- src/components/Pages/app/P5app.tsx
+++ src/components/Pages/app/P5app.tsx
@@ -1,13 +1,12 @@
 import './P5app.css';
 import Sketch from 'react-p5';
-import * as ml5 from 'ml5' ; 
-// import * as ml5 from 'ml5';
+import * as ml5 from 'ml5';
 
 import p5Types from "p5";
 
 export default () => {
   let video: p5Types.Element;
-  let posenet: ml5.PoseNet;
+  let posenet ; 
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     p5.createCanvas(window.screen.availWidth, window.screen.availHeight).parent(canvasParentRef);
@@ -18,7 +17,6 @@ export default () => {
 
   const modelLoaded = () => {
     console.log('model loaded');
-    // alert('model loaded' ) ;
   }
 
   const draw = (p5: p5Types) => {
@@ -27,35 +25,3 @@ export default () => {
 
   return <Sketch setup={setup} draw={draw} />;
 }
-
-// import './P5app.css';
-// import Sketch from 'react-p5';
-// import * as ml5 from 'ml5'
-
-// import p5Types from "p5";
-
-
-
-// export default () => {
-    
-//     let video ; 
-//     let posenet ; 
-    
-//     const setup = (p5: p5Types, canvasParentRef: Element) =>{
-//         p5.createCanvas(window.screen.availWidth , window.screen.availHeight ).parent(canvasParentRef);
-//         video = p5.createCapture(p5.VIDEO);
-//         video.hide();
-//         posenet = ml5.poseNet(video , modelLoaded ) ;
-        
-//     }
-
-//     const modelLoaded = () =>{
-//         console.log('model loaded')
-//     }
-
-//     const draw = (p5: p5Types) =>{
-//         p5.image(video , 0 , 0 );
-//     }
-
-//     return <Sketch setup={setup} draw={draw} ></Sketch>
-// }
