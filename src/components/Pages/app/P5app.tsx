79b6c2994a0cf9d55e1818bb5ecf5438c3a6ade1
amit dubey <amitdubey6261@gmail.com>
2023-05-15 23:28:18 +0530

    bug-cam

--- src/components/Pages/app/P5app.tsx
+++ src/components/Pages/app/P5app.tsx
@@ -1,32 +1,61 @@
 import './P5app.css';
 import Sketch from 'react-p5';
-import * as ml5 from 'ml5'
+import * as ml5 from 'ml5' ; 
+// import * as ml5 from 'ml5';
 
 import p5Types from "p5";
 
-
-
 export default () => {
-    
-    let video: any ; 
-    let posenet ; 
-    
-    const setup = (p5: p5Types, canvasParentRef: Element) =>{
-        p5.createCanvas(window.screen.availWidth , window.screen.availHeight ).parent(canvasParentRef);
-        video = p5.createCapture(p5.VIDEO);
-        video.hide();
-        posenet = ml5.poseNet(video , modelLoaded ) ;
-        
-    }
+  let video: p5Types.Element;
+  let posenet: ml5.PoseNet;
 
-    const modelLoaded = () =>{
-        console.log('model loaded')
-    }
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(window.screen.availWidth, window.screen.availHeight).parent(canvasParentRef);
+    video = p5.createCapture(p5.VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+  }
 
-    const draw = (p5: p5Types) =>{
-        p5.image(video , 0 , 0 );
-        console.log(video)
-    }
+  const modelLoaded = () => {
+    console.log('model loaded');
+    // alert('model loaded' ) ;
+  }
 
-    return <Sketch setup={setup} draw={draw} ></Sketch>
+  const draw = (p5: p5Types) => {
+    p5.image(video, 0, 0);
+  }
+
+  return <Sketch setup={setup} draw={draw} />;
 }
+
+// import './P5app.css';
+// import Sketch from 'react-p5';
+// import * as ml5 from 'ml5'
+
+// import p5Types from "p5";
+
+
+
+// export default () => {
+    
+//     let video ; 
+//     let posenet ; 
+    
+//     const setup = (p5: p5Types, canvasParentRef: Element) =>{
+//         p5.createCanvas(window.screen.availWidth , window.screen.availHeight ).parent(canvasParentRef);
+//         video = p5.createCapture(p5.VIDEO);
+//         video.hide();
+//         posenet = ml5.poseNet(video , modelLoaded ) ;
+        
+//     }
+
+//     const modelLoaded = () =>{
+//         console.log('model loaded')
+//     }
+
+//     const draw = (p5: p5Types) =>{
+//         p5.image(video , 0 , 0 );
+//     }
+
+//     return <Sketch setup={setup} draw={draw} ></Sketch>
+// }
